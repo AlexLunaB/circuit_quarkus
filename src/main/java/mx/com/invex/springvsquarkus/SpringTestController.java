387320c9deee78b957b6ce59265cadac0eb15036
Sergio Luna <alexlb642@gmail.com>
2026-03-19 00:44:26 -0600

    Add Dockerfile and new REST endpoints in SpringTestController

--- src/main/java/mx/com/invex/springvsquarkus/SpringTestController.java
+++ src/main/java/mx/com/invex/springvsquarkus/SpringTestController.java
@@ -3,6 +3,7 @@ package mx.com.invex.springvsquarkus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -11,14 +12,33 @@ import java.util.concurrent.CompletableFuture;
 public class SpringTestController {
 
     private final PythonServiceClient pythonClient;
-
+    private final RestClient restClient = RestClient.create();
     // Inyección de dependencias por constructor (recomendada)
     public SpringTestController(PythonServiceClient pythonClient) {
         this.pythonClient = pythonClient;
+    }
+
+    @GetMapping("/pokemon")
+    public String fetchPokemon() {
+        // Llamada a la PokéAPI (ej. Pikachu)
+        return restClient.get()
+                .uri("https://pokeapi.co/api/v2/pokemon/25")
+                .retrieve()
+                .body(String.class);
     }
 
     @GetMapping("/test")
     public CompletableFuture<String> testResilience() {
         return pythonClient.getFlakyData();
     }
+    @GetMapping("/stress")
+    public String stressTest() {
+        double result = 0;
+
+        for (int i = 0; i < 500000; i++) {
+            result += Math.sqrt(i);
+        }
+        return "Spring Boot OK: " + result;
+    }
+
 }
